aa08f787db52f1654c71f61f7dfd70f28f5614fc
burntcarrot <aadhav.n1@gmail.com>
2023-05-18 18:19:22 +0530

    fix: show preload count in logs

--- internal/module/preload.go
+++ internal/module/preload.go
@@ -2,6 +2,7 @@ package module
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -13,6 +14,8 @@ func Preload(m *ModuleLoader) error {
 		return fmt.Errorf("failed to read module directory: %w", err)
 	}
 
+	preloadCount := 0
+
 	for _, file := range moduleFiles {
 		if !file.IsDir() && filepath.Ext(file.Name()) == ".wasm" {
 			moduleName := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
@@ -20,8 +23,12 @@ func Preload(m *ModuleLoader) error {
 			if _, err := m.LoadModule(moduleName); err != nil {
 				return fmt.Errorf("failed to load module %s: %w", moduleName, err)
 			}
+
+			preloadCount++
 		}
 	}
+
+	log.Printf("preloaded %d modules.\n", preloadCount)
 
 	return nil
 }
